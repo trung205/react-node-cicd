31c564924bbd77cc56ccdf54f9e391a2c3f66e78
trunghoang <trunght@nal.vn>
2023-03-01 13:29:39 +0700

    create pj

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import './App.css';
+
+function App() {
+  const [username, setUsername] = useState('')
+  useEffect(() => {
+    getNames()
+  }, [])
+
+  const getNames = async () => {
+    const res = await axios.get('/names')
+    setUsername(res.data)
+  }
+  return (
+    <>
+      <h1>My frontend</h1>
+      <h3>{username}</h3>
+    </>
+  );
+}
+
+export default App;
